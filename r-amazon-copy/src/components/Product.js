85d850237250766b649fae064b1309510cdf6d04
Marlon Clay <luciousmc@icloud.com>
2021-05-27 08:58:53 -0700

    finish homepage ui

--- r-amazon-copy/src/components/Product.js
+++ r-amazon-copy/src/components/Product.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { StarIcon } from '@heroicons/react/solid';
 import { useState } from 'react'; 
+import Currency from 'react-currency-formatter';
 
 const MIN_RATING = 1;
 const MAX_RATING = 5;
@@ -12,26 +13,40 @@ function Product({ id, title, price, description, category, image }) {
   const [hasPrime] = useState(Math.random() < 0.5);
   
   return (
-    <div>
-      <p>{category}</p>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
 
       <Image src={image} height={200} width={200} objectFit="contain" />
 
-      <h4>{title}</h4>
+      <h4 className="my-3">{title}</h4>
 
       <div className="flex">
         {
           Array(rating).fill()
             .map((_, i) => {
-              return <StarIcon className="h-5" />
+              return <StarIcon className="h-5 text-yellow-500" />
             })
         }
       </div>
 
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
 
-      
+      <div>
+        <Currency quantity={price} className="mb-5" />
+      </div>
+
+      {hasPrime && (
+          <div className="flex items-center space-x-2 mt-5">
+            <img className="w-12" src="https://links.papareact.com/fdw" alt="Prime Logo" />
+            <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+          </div>
+        )}
+
+        <button className="mt-auto button">Add to Basket</button>
     </div>
   )
 }
 
-export default Product
+export default Product;

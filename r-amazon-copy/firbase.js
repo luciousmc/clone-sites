032c76ba7dec5664d1af7eeef70382f4dd015c54
Marlon Clay <luciousmc@icloud.com>
2021-06-01 13:10:11 -0700

    order component created pulled firebase DB data with Stripe order Info

--- r-amazon-copy/firbase.js
+++ r-amazon-copy/firbase.js
@@ -8,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "163726438781",
   appId: "1:163726438781:web:695d09356dce7959761c14"
 };
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+  const db = app.firestore();
+
+  export default db;
